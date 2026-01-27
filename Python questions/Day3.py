d133eac0f560b636c10a2fc3c9c13999bb26b7ae
Bhavya jain <Bhavya.24441@stu.upes.ac.in>
2026-01-27 21:05:43 +0530

    Day3

---
+++ Python questions/Day3.py
@@ -0,0 +1,74 @@
+#Bhavya Jain 590024441
+#Question 1: Count Positive and Negative Numbers in a List
+#Solution:
+# numbers = [10, -5, 3, -1, 0, 7, -2]
+# postive_count = 0
+# negative_count = 0
+# for i in numbers:
+#     if i > 0:
+#         postive_count = postive_count + 1
+#     elif i< 0:
+#         negative_count = negative_count + 1
+# print("Positive numbers:", postive_count)
+# print("Negative numbers:", negative_count)
+
+
+
+
+
+
+
+
+
+
+#Question 2 : Find sum of odd numbers in a list
+#Solution:
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# odd_sum = 0
+# for i in numbers:
+#     if i % 2 != 0:
+#         odd_sum = odd_sum +i
+# print("Sum of odd numbers:", odd_sum)
+
+
+
+
+
+
+
+
+#Question 3 : Convert string to uppercase
+#Solution:
+# string = "hello world"
+# uppercase_string = string.upper()
+# print("Uppercase string:", uppercase_string)
+
+
+
+
+
+#Question 4: Check if a number is divisible by both 3 and 5
+#Solution:
+# number = 15
+# if number % 3 == 0 and number % 5 == 0:
+#     print(number, "is divisible by both 3 and 5")
+# else:   
+#     print(number, "is not divisible by both 3 and 5")
+
+
+
+
+
+
+
+
+
+
+#Question 5: Count characters in a string (excluding spaces)
+#Solution:
+# string = " Hello World"
+# char_count = 0
+# for char in string:
+#     if char!=" ":
+#         char_count = char_count + 1
+# print("Number of characters (excluding spaces):", char_count)
