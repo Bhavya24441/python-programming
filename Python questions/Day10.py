768a6d12274684e96a4022b5762fe3118a536884
Bhavya jain <Bhavya.24441@stu.upes.ac.in>
2026-02-04 15:58:53 +0530

    lab

---
+++ Python questions/Day10.py
@@ -0,0 +1,26 @@
+#Q1- Find the most frequent character in a string 
+text = "programming"
+
+
+#Q2- Find all pairs that sum to 10
+numbers = [2,3,4,6,8,5,5]
+
+
+
+
+#Q3- Check if a sentence if pangram
+text = "the quick brown fox jumps over the lazy dog"
+
+
+
+
+#Q4- compress a string
+text = "aaabbaccdasdfddd"
+
+
+
+#Q5- Find common characters in two strings
+s1 = "python"
+s2 = "typhoon"
+
+
